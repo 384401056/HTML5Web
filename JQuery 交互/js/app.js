008cbed45c1d04f45a2ad3d00cb5a81e8d820593
384401056 <384401056@qq.com>
2015-11-10 17:35:03 +0800

    JQuery 交互

---
+++ JQuery 交互/js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2015/11/3.
+ */
+$(document).ready(function(){
+    $("#div01").draggable();
+    $("#div02").draggable();
+    $("#div03").draggable();
+});
--- JQuery/JQuery UI/js/app.js
+++ JQuery/JQuery UI/js/app.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2015/11/3.
  */
 $(document).ready(function(){
-    $( "#a_btn" ).button();
+    $("#a_btn" ).button();
     $("#progressbar").progressbar({value:55});
     $("#accordion").accordion();
     $("#datepicker").datepicker();
